f9423811b9f54c28f507db259b3dd0ec223a8593
tiffany1618 <tiffany9342@gmail.com>
2020-07-23 19:55:02 -0700

    Tried PID

--- path_follower.ino
+++ path_follower.ino
@@ -14,7 +14,7 @@ const int LED_BL = 57;
 const int LED_BR = 58;
 
 // PID Terms
-const float K_P = 0.0;
+const float K_P = 0.01;
 const float K_I = 0.0;
 const float K_D = 0.0;
 
@@ -26,9 +26,9 @@ int BASE_SPEED = 60;
 uint16_t sensor_max_vals[] = {1916, 1813, 1903, 1232, 1321, 1870, 1657, 1715};
 uint16_t sensor_min_vals[] = {576, 537, 597, 620, 506, 630, 597, 785};
 uint16_t sensor_vals[NUM_SENSORS];
-float turn_angle; // Degrees
-int left_speed;
-int right_speed;
+float speed_change;
+int current_time;
+int time_per_loop;
 float curr_error;
 float prev_error = 0;
 float total_error = 0;
@@ -54,6 +54,8 @@ void setup() {
 
 
 void loop() {
+  current_time = millis();
+  
   // Read raw sensor values
   ECE3_read_IR(sensor_vals);
 
@@ -63,12 +65,13 @@ void loop() {
     curr_error += (((sensor_vals[i] - sensor_min_vals[i]) * 1000) / sensor_max_vals[i]) * WEIGHTS_8421_4[i] / WEIGHTS_8421_4[NUM_SENSORS];
   }
 
-  turn_angle = K_P * curr_error + K_I * total_error + K_D * (curr_error - prev_error);
+  time_per_loop = millis() - current_time;
+  speed_change = K_P * curr_error + K_I * total_error + K_D * ((curr_error - prev_error) / time_per_loop);
 
-  analogWrite(LEFT_PWM_PIN, left_speed);
-  analogWrite(RIGHT_PWM_PIN, right_speed);
+  analogWrite(LEFT_PWM_PIN, BASE_SPEED - speed_change);
+  analogWrite(RIGHT_PWM_PIN, BASE_SPEED + speed_change);
 
-  total_error += curr_error;
+  total_error += curr_error * time_per_loop;
   prev_error = curr_error;
 
   //  // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
