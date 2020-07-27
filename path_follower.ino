4b3549d3ef8c252d2acab76b61a3428eef2855e2
tiffany1618 <tiffany9342@gmail.com>
2020-07-26 21:11:57 -0700

    Added PID_Controller library

--- path_follower.ino
+++ path_follower.ino
@@ -1,3 +1,4 @@
+#include <PID_Controller.h>
 #include <ECE3.h>
 
 // Pin values
@@ -14,10 +15,10 @@ const int LED_BL = 57;
 const int LED_BR = 58;
 
 // PID Terms
-const float K_P = 0.01;
-const float K_I = 0.0;
-const float K_D = 0.0;
-const float ERROR_THRESHOLD = 100;
+const double K_P = 0.01;
+const double K_I = 0.0;
+const double K_D = 0.0;
+const double ERROR_THRESHOLD = 100;
 
 const int NUM_SENSORS = 8;
 float WEIGHTS_8421_4[] = { -8, -4, -2, -1, 1, 2, 4, 8, 4};
@@ -27,12 +28,10 @@ const int BASE_SPEED = 60;
 uint16_t sensor_max_vals[] = {1916, 1813, 1903, 1232, 1321, 1870, 1657, 1715};
 uint16_t sensor_min_vals[] = {576, 537, 597, 620, 506, 630, 597, 785};
 uint16_t sensor_vals[NUM_SENSORS];
-float speed_change;
-int current_time;
-int time_per_loop;
-float curr_error;
-float prev_error = 0;
-float total_error = 0;
+double speed_change;
+double error;
+
+PID_Controller pid(&error, &speed_change, ERROR_THRESHOLD, K_P, K_I, K_D);
 
 void setup() {
    ECE3_Init();
@@ -52,40 +51,32 @@ void setup() {
 
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
 
-  min_calibrate();
-  delay(5000);
+//  min_calibrate();
+//  delay(5000);
 //  max_calibrate();
 //  delay(3000);
+
+  speed_change = 0;
+  pid.set_time();
 }
 
 
 void loop() {
-  current_time = millis();
-  
   // Read raw sensor values
   ECE3_read_IR(sensor_vals);
 
   // Calculate error
-  curr_error = 0;
+  error = 0;
   for (int i = 0; i < NUM_SENSORS; i++) {
-    curr_error += (((sensor_vals[i] - sensor_min_vals[i]) * 1000) / sensor_max_vals[i]) * WEIGHTS_8421_4[i] / WEIGHTS_8421_4[NUM_SENSORS];
+    error += (((sensor_vals[i] - sensor_min_vals[i]) * 1000) / sensor_max_vals[i]) * WEIGHTS_8421_4[i] / WEIGHTS_8421_4[NUM_SENSORS];
   }
 
-  Serial.print(curr_error);
-  Serial.print('\n');
+  Serial.println(error);
 
-  if (curr_error < ERROR_THRESHOLD) {
-    curr_error = 0;
-  }
-
-  time_per_loop = millis() - current_time;
-  speed_change = K_P * curr_error + K_I * total_error + K_D * ((curr_error - prev_error) / time_per_loop);
+   pid.calculate();
 
   analogWrite(LEFT_PWM_PIN, BASE_SPEED - speed_change);
   analogWrite(RIGHT_PWM_PIN, BASE_SPEED + speed_change);
-
-  total_error += curr_error * time_per_loop;
-  prev_error = curr_error;
 }
 
 void reset(uint16_t *vals) {
