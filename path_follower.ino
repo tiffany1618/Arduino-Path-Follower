32c4fbbcead73794f597bd7d854c0d8b3a169ed9
tiffany1618 <tiffany9342@gmail.com>
2020-08-05 18:56:16 -0700

    Added serial input

--- path_follower.ino
+++ path_follower.ino
@@ -18,43 +18,32 @@ const int LED_FR = 41;
 const int LED_BL = 57;
 const int LED_BR = 58;
 
-const int BUMP_0 = 24;
-const int BUMP_1 = 25;
-const int BUMP_2 = 6;
-const int BUMP_3 = 27;
-const int BUMP_4 = 8;
-const int BUMP_5 = 28;
-
 // PID Terms
-double K_P = 0.025;
+const double K_P = 0.05;
 const double K_I = 0.0;
-const double K_D = 0.0;
+const double K_D = 4.9;
 const double ERROR_THRESHOLD = 0; // Set error to 0 if the absolute value of the error is less than this threshold, to prevent small oscillations
 
 // Encoder values
-//const int STRAIGHT_COUNT;
-//const int RIBBON_COUNT;
+//const int STRAIGHT_COUNT_LEFT = ;
+//const int STRAIGHT_COUNT_RIGHT = ;
+//const int RIBBON_COUNT_LEFT = ;
+//const int RIBBON_COUNT_RIGHT = ;
 const int ABOUT_FACE_COUNT_LEFT = 330;
 
 // Other constants
 const int NUM_SENSORS = 8; // Number of sensors on the car
-int BASE_SPEED = 150; // Default wheel speed
+const int BASE_SPEED = 130; // Default wheel speed
 const int SENSOR_THRESHOLD_WHITE = 100;
 const int SENSOR_THRESHOLD_DIFF = 300;
 
 // Weighting schemes for sensor fusion. Last number is the divisor.
-float WEIGHTS_8421_4[] = { -8, -4, -2, -1, 1, 2, 4, 8, 4};
-float WEIGHTS_1514128_8[] = { -15, -14, -12, -8, 8, 12, 14, 15, 8};
-float WEIGHTS_LINEAR[] = {-1, -1, -1, -1, 1, 1, 1, 1, 1};
+double WEIGHTS_8421_4[] = {-8, -4, -2, -1, 1, 2, 4, 8, 4};
+double WEIGHTS_1514128_8[] = {-15, -14, -12, -8, 8, 12, 14, 15, 8};
+double WEIGHTS_LINEAR[] = {-4, -3, -2, -1, 1, 2, 3, 4, 4};
 
-// 11 am readings
-//int sensor_max_vals[] = {1916, 1813, 1903, 1232, 1321, 1870, 1657, 1715};
-//int sensor_min_vals[] = {576, 537, 597, 620, 506, 630, 597, 785};
-// 6 pm readings
-int sensor_max_vals[] = {2500, 2500, 2500, 2453, 2353, 2500, 2500, 2500};
-int sensor_min_vals[] = {576, 555, 613, 622, 542, 647, 613, 756};
-//int sensor_max_vals[NUM_SENSORS];
-//int sensor_min_vals[NUM_SENSORS];
+int sensor_max_vals[NUM_SENSORS];
+int sensor_min_vals[NUM_SENSORS];
 uint16_t sensor_vals[NUM_SENSORS];
 
 double norm_val; // Stores normalized sensor value
@@ -66,15 +55,11 @@ double sensor_sum;
 double sensor_min;
 double sensor_max;
 
-// Keep track of button states
-bool right_button_state;
-bool left_button_state;
-
 // Initialize PID controller
 PID_Controller pid(&error, &speed_change, ERROR_THRESHOLD, K_P, K_I, K_D);
 
-// Bumper switches
-Bump_Switch bump_switch[6];
+char serialChar;
+double serialNum;
 
 void setup() {
   ECE3_Init();
@@ -89,22 +74,12 @@ void setup() {
   pinMode(LEFT_BUTTON, INPUT_PULLUP);
   pinMode(RIGHT_BUTTON, INPUT_PULLUP);
 
-  bump_switch[0].begin(BUMP_0, INPUT_PULLUP);
-  bump_switch[1].begin(BUMP_1, INPUT_PULLUP);
-  bump_switch[2].begin(BUMP_2, INPUT_PULLUP);
-  bump_switch[3].begin(BUMP_3, INPUT_PULLUP);
-  bump_switch[4].begin(BUMP_4, INPUT_PULLUP);
-  bump_switch[5].begin(BUMP_5, INPUT_PULLUP);
-
   digitalWrite(LEFT_NSLP_PIN, HIGH);
   digitalWrite(RIGHT_NSLP_PIN, HIGH);
   digitalWrite(LEFT_DIR_PIN, LOW);
   digitalWrite(RIGHT_DIR_PIN, LOW);
 
   Serial.begin(9600); // Set the data rate in bits per second for serial data transmission
-
-  right_button_state = false;
-  left_button_state = false;
 }
 
 
@@ -117,35 +92,28 @@ void loop() {
   if (digitalRead(LEFT_BUTTON) == LOW) {
     follow_path();
   }
-  
-  // Get bumper switch input (for PID tuning)
-  for (int i = 0; i < 6; i++) {
-    if (bump_switch[i].read() == 0) {
-      switch(i) {
-        case 0:
-          K_P += 0.005;
-          blink_twice(LED_FR);
-          break;
-        case 1:
-          K_P -= 0.005;
-          blink_twice(LED_FL);
-          break;
-        case 2:
-          K_P += 0.001;
-          blink_twice(LED_FR);
-          break;
-        case 3:
-          K_P -= 0.001;
-          blink_twice(LED_FL);
-          break;
-        case 4:
-          BASE_SPEED += 10;
-          blink_twice(LED_FR);
-          break;
-        case 5:
-          break;
-      }
+
+  // Get PID input from serial monitor
+  if (Serial.available() > 0) {
+    serialChar = Serial.read();
+    serialNum = (double) Serial.parseFloat();
+
+    switch (serialChar) {
+      case 'P':
+        pid.set_gains(serialNum, K_I, K_D);
+        break;
+      case 'I':
+        pid.set_gains(K_P, serialNum, K_D);
+        break;
+      case 'D':
+        pid.set_gains(K_P, K_I, serialNum);
+        break;
     }
+
+    Serial.print("K_P: ");
+    Serial.print(pid.get_K_P(), 5);
+    Serial.print(", K_D: ");
+    Serial.println(pid.get_K_D(), 5);
   }
 }
 
@@ -162,13 +130,19 @@ void run_calibration() {
 
 void follow_path() {
   Serial.print("K_P: ");
-  Serial.println(K_P);
+  Serial.print(pid.get_K_P(), 5);
+  Serial.print(", K_D: ");
+  Serial.print(pid.get_K_D(), 5);
+  Serial.print(", BASE_SPEED: ");
+  Serial.println(BASE_SPEED);
+
+  bool drive = true;
 
   delay_milli(1000);
   digitalWrite(LED_FL, HIGH);
   reset_car();
-  
-  while (true) {
+ 
+  while (drive) {
     // Read raw sensor values
     ECE3_read_IR(sensor_vals);
 
@@ -178,8 +152,8 @@ void follow_path() {
     sensor_min = 2500;
     sensor_max = 0;
     for (int i = 0; i < NUM_SENSORS; i++) {
-      norm_val = ((sensor_vals[i] - sensor_min_vals[i]) * 1000) / sensor_max_vals[i];
-      error += norm_val * WEIGHTS_8421_4[i] / WEIGHTS_8421_4[NUM_SENSORS];
+      norm_val = (((double) sensor_vals[i] - sensor_min_vals[i]) * 1000) / sensor_max_vals[i];
+      error += norm_val * WEIGHTS_LINEAR[i] / WEIGHTS_LINEAR[NUM_SENSORS];
 
       sensor_sum += norm_val;
       if (sensor_min > sensor_vals[i]) {
@@ -192,11 +166,10 @@ void follow_path() {
     
     if ((sensor_sum <= SENSOR_THRESHOLD_WHITE) && (sensor_max - sensor_min <= SENSOR_THRESHOLD_DIFF)) {
       set_speed(0);
-      toggle_state(&left_button_state, LED_FL);
-    } 
-    else {
+      drive = false;
+    } else {
 //      pid.calculate();
-        pid.tune();
+      pid.tune();
 
 //        Serial.print(getEncoderCount_left());
 //        Serial.print(", ");
@@ -208,7 +181,7 @@ void follow_path() {
     }
   }
 
-  digitalWrite(LED_FR, LOW);
+  digitalWrite(LED_FL, LOW);
 }
 
 // Reset PID and Encoder values
@@ -249,7 +222,7 @@ void calibrate(int *vals) {
     ECE3_read_IR(sensor_vals);
 
     for (int j = 0; j < NUM_SENSORS; j++) {
-      vals[j] += sensor_vals[j] / 10;
+      vals[j] += (double) sensor_vals[j] / 10;
     }
   }
 
@@ -260,17 +233,6 @@ void calibrate(int *vals) {
 void zero(int *vals) {
   for (int i = 0; i < NUM_SENSORS; i++) {
     vals[i] = 0;
-  }
-}
-
-// Toggle state of button and corresponding LED indicator
-void toggle_state(bool *state, int led_pin) {
-  if (*state) {
-    *state = false;
-    digitalWrite(led_pin, LOW);
-  } else {
-    *state = true;
-    digitalWrite(led_pin, HIGH);
   }
 }
 
